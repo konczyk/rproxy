bf2bf1fa7269495355cf0d4df7b655d37887edd4
Daniel Kończyk <daniel@konczyk.net>
2026-01-15 09:40:01 +0100

    Add test for 403 error

--- tests/proxy_test.rs
+++ tests/proxy_test.rs
@@ -4,19 +4,25 @@ use std::sync::Arc;
 use tokio::net::{TcpListener, TcpStream};
 
 mod tests {
-    use std::time::Duration;
+    use std::collections::HashSet;
+    use std::net::IpAddr;
+    use std::str::FromStr;
     use super::*;
-    use rproxy::config::Config;
+    use rproxy::config::{AccessControl, Config};
+    use std::time::Duration;
     use tokio::io::{AsyncReadExt, AsyncWriteExt};
     use tokio::time::sleep;
 
-    async fn connect(routes: Vec<Route>) -> TcpStream {
+    async fn connect(routes: Vec<Route>, access: Option<AccessControl>) -> TcpStream {
         let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
         let addr = listener.local_addr().unwrap();
         let c = "tests/config.toml".to_string();
 
         let mut c = Config::new(&c).unwrap();
         c.add_routes(routes);
+        if let Some(a) = access {
+            c.access = a;
+        }
         let config = Arc::new(c);
 
         tokio::spawn(async move {
@@ -30,7 +36,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_400_on_missing_headers_delimiter() {
-        let mut client = connect(vec![]).await;
+        let mut client = connect(vec![], None).await;
         client.write_all(b"GET /buba HTTP/1.1\r\nHost: localhost:8080\r\n").await.unwrap();
         client.shutdown().await.unwrap();
 
@@ -42,7 +48,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_404_on_missing_route() {
-        let mut client = connect(vec![]).await;
+        let mut client = connect(vec![], None).await;
         client.write_all(b"GET /buba HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
         client.shutdown().await.unwrap();
 
@@ -54,7 +60,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_400_on_malformed_request_line() {
-        let mut client = connect(vec![]).await;
+        let mut client = connect(vec![], None).await;
         client.write_all(b"xxx\r\n\r\n").await.unwrap();
         client.shutdown().await.unwrap();
 
@@ -65,6 +71,21 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn test_403_on_non_whitelisted_ip() {
+        let access = AccessControl {
+            whitelist: Some(HashSet::from([IpAddr::from_str("1.1.1.1").unwrap()])),
+        };
+        let mut client = connect(vec![], Some(access)).await;
+        client.write_all(b"GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
+        client.shutdown().await.unwrap();
+
+        let mut response = [0u8; 1024];
+        client.read(&mut response).await.unwrap();
+
+        assert!(str::from_utf8(&response).unwrap().contains("403 Forbidden"));
+    }
+
+    #[tokio::test]
     async fn test_502_on_invalid_gateway() {
         let routes = vec![Route {
             host: "localhost:54322".as_bytes().to_vec(),
@@ -72,7 +93,7 @@ mod tests {
             addr: "localhost:54321".to_string(),
             timeout: None
         }];
-        let mut client = connect(routes).await;
+        let mut client = connect(routes, None).await;
         client.write_all(b"GET /target HTTP/1.1\r\nHost: localhost:54322\r\n\r\n").await.unwrap();
         client.shutdown().await.unwrap();
 
@@ -102,7 +123,7 @@ mod tests {
             addr: backend_addr.to_string(),
             timeout: Some(100)
         }];
-        let mut client = connect(routes).await;
+        let mut client = connect(routes, None).await;
 
         client.write_all(b"GET /target HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
         client.shutdown().await.unwrap();
@@ -133,7 +154,7 @@ mod tests {
             addr: backend_addr.to_string(),
             timeout: None
         }];
-        let mut client = connect(routes).await;
+        let mut client = connect(routes, None).await;
 
         client.write_all(b"GET /target HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
         client.shutdown().await.unwrap();
