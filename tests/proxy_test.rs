06693d8cf72e497f070c36085b37990d707c1c0c
Daniel Kończyk <daniel@konczyk.net>
2026-01-15 23:58:31 +0100

    Add integration tests for authorization

--- tests/proxy_test.rs
+++ tests/proxy_test.rs
@@ -5,11 +5,13 @@ use tokio::net::{TcpListener, TcpStream};
 
 mod tests {
     use super::*;
-    use rproxy::config::{AccessControl, Config};
-    use std::collections::HashSet;
+    use rproxy::config::{AccessControl, Auth, Config};
+    use std::collections::{HashMap, HashSet};
     use std::net::IpAddr;
     use std::str::FromStr;
     use std::time::Duration;
+    use base64::Engine;
+    use base64::prelude::BASE64_STANDARD;
     use tokio::io::{AsyncReadExt, AsyncWriteExt};
     use tokio::time::sleep;
 
@@ -33,6 +35,119 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn test_200_on_successful_proxy_to_backend() {
+        let backend_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let backend_addr = backend_listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            if let Ok((mut stream, _)) = backend_listener.accept().await {
+                let mut buf = [0u8; 1024];
+                let _ = stream.read(&mut buf).await;
+                let response = b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
+                let _ = stream.write_all(response).await;
+            }
+        });
+
+        let routes = vec![Route {
+            host: "localhost:8080".as_bytes().to_vec(),
+            path: "/target".as_bytes().to_vec(),
+            addr: backend_addr.to_string(),
+            timeout: None
+        }];
+        let mut client = connect(routes, None).await;
+
+        client.write_all(b"GET /target HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
+        client.shutdown().await.unwrap();
+
+        let mut response = String::new();
+        client.read_to_string(&mut response).await.unwrap();
+
+        assert!(response.contains("200 OK"));
+        assert!(response.contains("Hello"));
+    }
+
+    #[tokio::test]
+    async fn test_200_on_successful_proxy_to_backend_with_api_key() {
+        let backend_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let backend_addr = backend_listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            if let Ok((mut stream, _)) = backend_listener.accept().await {
+                let mut buf = [0u8; 1024];
+                let _ = stream.read(&mut buf).await;
+                let response = b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
+                let _ = stream.write_all(response).await;
+            }
+        });
+
+        let routes = vec![Route {
+            host: "localhost:8080".as_bytes().to_vec(),
+            path: "/target".as_bytes().to_vec(),
+            addr: backend_addr.to_string(),
+            timeout: None
+        }];
+
+        let access = AccessControl {
+            whitelist: None,
+            auth: Some(Auth {
+                users: None,
+                api_keys: Some(HashSet::from(["api_key1".to_string()])),
+            }),
+        };
+
+        let mut client = connect(routes, Some(access)).await;
+        client.write_all(b"GET /target HTTP/1.1\r\nHost: localhost:8080\r\nX-Api-Key: api_key1\r\n\r\n").await.unwrap();
+        client.shutdown().await.unwrap();
+
+        let mut response = String::new();
+        client.read_to_string(&mut response).await.unwrap();
+
+        assert!(response.contains("200 OK"));
+        assert!(response.contains("Hello"));
+    }
+
+    #[tokio::test]
+    async fn test_200_on_successful_proxy_to_backend_with_basic_auth() {
+        let backend_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let backend_addr = backend_listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            if let Ok((mut stream, _)) = backend_listener.accept().await {
+                let mut buf = [0u8; 1024];
+                let _ = stream.read(&mut buf).await;
+                let response = b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
+                let _ = stream.write_all(response).await;
+            }
+        });
+
+        let routes = vec![Route {
+            host: "localhost:8080".as_bytes().to_vec(),
+            path: "/target".as_bytes().to_vec(),
+            addr: backend_addr.to_string(),
+            timeout: None
+        }];
+
+        let encoded = BASE64_STANDARD.encode("admin:password");
+        let access = AccessControl {
+            whitelist: None,
+            auth: Some(Auth {
+                users: Some(HashMap::from([("admin".to_string(), "password".to_string())])),
+                api_keys: None,
+            }),
+        };
+
+        let mut client = connect(routes, Some(access)).await;
+        client.write_all(format!("GET /target HTTP/1.1\r\nHost: localhost:8080\r\nAuthorization: Basic {encoded}\r\n\r\n").as_bytes()).await.unwrap();
+        client.shutdown().await.unwrap();
+
+        let mut response = String::new();
+        client.read_to_string(&mut response).await.unwrap();
+
+        assert!(response.contains("200 OK"));
+        assert!(response.contains("Hello"));
+    }
+
+    #[tokio::test]
     async fn test_400_on_missing_headers_delimiter() {
         let mut client = connect(vec![], None).await;
         client.write_all(b"GET /buba HTTP/1.1\r\nHost: localhost:8080\r\n").await.unwrap();
@@ -45,18 +160,6 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_404_on_missing_route() {
-        let mut client = connect(vec![], None).await;
-        client.write_all(b"GET /buba HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
-        client.shutdown().await.unwrap();
-
-        let mut response = String::new();
-        client.read_to_string(&mut response).await.unwrap();
-
-        assert!(response.contains("404 Not Found"));
-    }
-
-    #[tokio::test]
     async fn test_400_on_malformed_request_line() {
         let mut client = connect(vec![], None).await;
         client.write_all(b"xxx\r\n\r\n").await.unwrap();
@@ -66,6 +169,49 @@ mod tests {
         client.read_to_string(&mut response).await.unwrap();
 
         assert!(response.contains("400 Bad Request"));
+    }
+
+    #[tokio::test]
+    async fn test_401_on_invalid_basic_auth() {
+        let encoded = BASE64_STANDARD.encode("admin:password1");
+        let access = AccessControl {
+            whitelist: None,
+            auth: Some(Auth {
+                users: Some(HashMap::from([("admin".to_string(), "password".to_string())])),
+                api_keys: None,
+            }),
+        };
+        let mut client = connect(vec![], Some(access)).await;
+        client.write_all(format!("GET / HTTP/1.1\r\nHost: localhost:8080\r\nAuthorization: Basic {encoded}\r\n\r\n").as_bytes()).await.unwrap();
+        client.shutdown().await.unwrap();
+
+        let mut response = [0u8; 1024];
+        client.read(&mut response).await.unwrap();
+
+        let res = str::from_utf8(&response).unwrap();
+        assert!(res.contains("401 Unauthorized"));
+        assert!(res.contains("WWW-Authenticate: Basic realm=\"Proxy\""));
+    }
+
+    #[tokio::test]
+    async fn test_401_on_invalid_api_key() {
+        let access = AccessControl {
+            whitelist: None,
+            auth: Some(Auth {
+                users: None,
+                api_keys: Some(HashSet::from(["api_key1".to_string()])),
+            }),
+        };
+        let mut client = connect(vec![], Some(access)).await;
+        client.write_all(b"GET / HTTP/1.1\r\nHost: localhost:8080\r\nX-Api-Key: api_key2\r\n\r\n").await.unwrap();
+        client.shutdown().await.unwrap();
+
+        let mut response = [0u8; 1024];
+        client.read(&mut response).await.unwrap();
+
+        let res = str::from_utf8(&response).unwrap();
+        assert!(res.contains("401 Unauthorized"));
+        assert!(!res.contains("WWW-Authenticate: Basic realm=\"Proxy\""));
     }
 
     #[tokio::test]
@@ -82,6 +228,18 @@ mod tests {
         client.read(&mut response).await.unwrap();
 
         assert!(str::from_utf8(&response).unwrap().contains("403 Forbidden"));
+    }
+
+    #[tokio::test]
+    async fn test_404_on_missing_route() {
+        let mut client = connect(vec![], None).await;
+        client.write_all(b"GET /buba HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
+        client.shutdown().await.unwrap();
+
+        let mut response = String::new();
+        client.read_to_string(&mut response).await.unwrap();
+
+        assert!(response.contains("404 Not Found"));
     }
 
     #[tokio::test]
@@ -133,35 +291,4 @@ mod tests {
         assert!(response.contains("504 Gateway Timeout"));
     }
 
-    #[tokio::test]
-    async fn test_successful_proxy_to_backend() {
-        let backend_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
-        let backend_addr = backend_listener.local_addr().unwrap();
-
-       tokio::spawn(async move {
-            if let Ok((mut stream, _)) = backend_listener.accept().await {
-                let mut buf = [0u8; 1024];
-                let _ = stream.read(&mut buf).await;
-                let response = b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
-                let _ = stream.write_all(response).await;
-            }
-        });
-
-        let routes = vec![Route {
-            host: "localhost:8080".as_bytes().to_vec(),
-            path: "/target".as_bytes().to_vec(),
-            addr: backend_addr.to_string(),
-            timeout: None
-        }];
-        let mut client = connect(routes, None).await;
-
-        client.write_all(b"GET /target HTTP/1.1\r\nHost: localhost:8080\r\n\r\n").await.unwrap();
-        client.shutdown().await.unwrap();
-
-        let mut response = String::new();
-        client.read_to_string(&mut response).await.unwrap();
-
-        assert!(response.contains("200 OK"));
-        assert!(response.contains("Hello"));
-    }
 }
