a612ed560446f76ad2f5ca73bc323db2d3603763
Daniel Kończyk <daniel@konczyk.net>
2026-01-17 21:42:34 +0100

    Add health check thresholds

--- src/health.rs
+++ src/health.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::TcpStream;
@@ -5,15 +6,28 @@ use tokio::sync::RwLock;
 use tracing::{error, info, trace};
 
 pub async fn check(path: String, backends: Vec<String>, active: Arc<RwLock<Vec<String>>>) {
-    let mut interval = tokio::time::interval(Duration::from_secs(5));
+    let mut interval = tokio::time::interval(Duration::from_secs(1));
+
+    let mut health_map = HashMap::from(
+        backends.iter().map(|b| (b.clone(), 3)).collect::<HashMap<String, isize>>()
+    );
 
     loop {
         interval.tick().await;
 
         let mut healthy = Vec::with_capacity(backends.len());
         for backend in &backends {
-            if let Ok(Ok(_)) = tokio::time::timeout(Duration::from_secs(1), TcpStream::connect(&backend)).await {
-                healthy.push(backend.clone());
+            let status = health_map.get_mut(backend).unwrap();
+            if let Ok(Ok(_)) = tokio::time::timeout(Duration::from_secs(1), TcpStream::connect(backend)).await {
+                *status = (*status + 1).min(3);
+                if *status > 0 {
+                    healthy.push(backend.clone());
+                }
+            } else {
+                *status = (*status - 1).max(-2);
+                if *status > -1 {
+                    healthy.push(backend.clone());
+                }
             }
         }
 
@@ -27,6 +41,5 @@ pub async fn check(path: String, backends: Vec<String>, active: Arc<RwLock<Vec<S
         }
 
         *current = healthy;
-
     }
 }
